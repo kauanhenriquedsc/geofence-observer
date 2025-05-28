bb1165de4953131d5e784c5021946dbeca8ed58a
kauanhenriquedsc <khenrique@datasmartcom.com.br>
2025-05-28 12:57:33 -0300

    add simple server and client app for telemetry a line geofence

---
+++ app/sms-service.js
@@ -0,0 +1,23 @@
+const twilio = require('twilio');
+
+
+const accountSid = '-';
+const authToken = '-';
+const twilioNumber = '-'; 
+
+const client = twilio(accountSid, authToken);
+
+async function sendSMS(to, message) {
+    try {
+        await client.messages.create({
+            body: message,
+            from: twilioNumber,
+            to: to
+        });
+        console.log('send sms', to);
+    } catch (err) {
+        console.error('erro for send sms:', err.message);
+    }
+}
+
+module.exports = { sendSMS };
